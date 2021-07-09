005d6fa1dc3ea97fd09eed3e9a1e1b3c418e9e53
Marco Panozzo <marco.panozzo@tecnicosuperiorekennedy.it>
2021-07-09 09:20:51 +0200

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  host: "https://polo-nord-backend.azurewebsites.net/api/",
+  endpoint: {
+    'articoli': 'articoli/',
+    'ordini': 'ordini/',
+    'acquisti': 'acquisti/',
+    'fornitori': 'fornitori/',
+    'carico': 'carico/',
+    'asin': 'asin/',
+    'titolo': 'titolo/',
+    'categoria' : 'categoria/',
+    'addLastOrders': 'addLastOrders/',
+    'prezzo': 'prezzo',
+    'allCategorie' : 'allCategorie',
+    'globalFilter': 'globalFilter',
+    'articoliAcquistati': 'articoliAcquistati/'
+  }
+};
